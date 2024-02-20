d96a34413bffa3c55b47537536b3e0ac6f51dc96
Roberto <Roberto@DESKTOP-1818MJE>
2024-02-19 20:35:24 -0600

    Agregar archivos de proyecto.

---
+++ Mathfunctions/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathfunctions
+{
+    using System;
+
+    public class MathFunctions
+    {
+        // Method to calculate the factorial of a non-negative integer
+        public static long Factorial(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Factorial is not defined for negative numbers.");
+
+            long result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+            }
+            return result;
+        }
+
+        // Method to generate a random integer within a specified range
+        public static int GenerateRandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max + 1);
+        }
+
+        // Method to find the greatest common divisor (GCD) of two integers
+        public static int GCD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return Math.Abs(a);
+        }
+    }
+
+}
---
+++ Mathfunctions/Program.cs
@@ -0,0 +1,12 @@
+﻿using Mathfunctions;
+
+int n = 5;
+Console.WriteLine($"Factorial of {n}: {MathFunctions.Factorial(n)}");
+
+int min = 1;
+int max = 100;
+Console.WriteLine($"Random number between {min} and {max}: {MathFunctions.GenerateRandomNumber(min, max)}");
+
+int a = 12;
+int b = 18;
+Console.WriteLine($"GCD of {a} and {b}: {MathFunctions.GCD(a, b)}");
